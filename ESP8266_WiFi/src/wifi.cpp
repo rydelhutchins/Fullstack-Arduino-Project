a75621c5794d8350ff7d397b301504bf092469fe
rydelhutchins <rydel.hutch@gmail.com>
2024-06-23 23:08:43 -0700

    small update

--- ESP8266_WiFi/src/wifi.cpp
+++ ESP8266_WiFi/src/wifi.cpp
@@ -1,8 +1,8 @@
 #include "wifi.h"
 
 // Define your WiFi credentials
-const char* ssid = "MagicWifi";
-const char* password = "&75eq1hh671D!";
+const char* ssid = "MagicWifi_Guest";
+const char* password = "houseguest";
 
 // Define the function to connect to WiFi
 void connectToWiFi() {
